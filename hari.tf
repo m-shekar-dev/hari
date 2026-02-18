b451df60d4833e4349e50bcdaf3950a3378aabd5
M Shekar <shekarmyadav321@gmail.com>
2026-02-18 21:27:11 +0530

    develoupre2

--- hari.tf
+++ hari.tf
@@ -4,3 +4,9 @@ resource "azurerm_virtual_network" "vn" {
   resource_group_name = azurerm_resource_group.rg.name
   address_space = [ "10.0.0.0/24" ]
 }
+resource "azurerm_subnet" "subnet"{
+    name = "sub1"
+    resource_group_name =  azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vn.name
+    address_prefixes = [ "10.1.0.0/24" ]
+}
